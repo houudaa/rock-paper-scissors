3674897e69a895c5b3e4c7991356336a018d1fc2
mhmoud-codes <mhmoud.programmer@gmail.com>
2023-11-03 02:04:36 +0200

    Delete unuseeful comments

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function getComputerChoice() {
 function playRound(playerSelection) {
     let result = 2, roundMsg = '';
     let computerSelection = getComputerChoice();
-    
+
     // checking if its a won or lose
     if(playerSelection === 'Rock') { result = (computerSelection === 'Scissors') ? 1 : 0; }
     else if(playerSelection === 'Paper') { result = (computerSelection === 'Rock') ? 1 : 0; }
@@ -65,70 +65,3 @@ function playRound(playerSelection) {
 playAgainBtn.addEventListener('click', () => {
     location.reload()
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// gameResultsMsgs.appendChild(playAgainBtn);
-
-// playAgainBtn.addEventListener('click', () => {
-//     gameResultsMsgs.innerHTML = '';
-//     game();
-// })
-
-
-
-    // 5 rounds of the game
-    // for(let i=0; i<5; i++)
-    // {
-
-    //     let playerSelection = prompt('Enter a Choice: ');
-
-    //     let computerSelection = getComputerChoice();
-
-    //     // converting the user choice to match the comparison 
-    //     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-
-    //     let result = playRound(playerSelection, computerSelection);
-
-    //     if(result === 1)
-    //     {
-    //         console.log(`You Won! ${playerSelection} beats ${computerSelection}`);
-    //         playerScore++;
-    //     }
-    //     else if(result === 0)
-    //     {
-    //         console.log(`You lost, ${playerSelection} is beaten by ${computerSelection}`);
-    //         computerScore++;
-    //     }
-    //     else
-    //     {
-    //         console.log(`It's a tie, ${playerSelection} ties with ${computerSelection}`)
-    //     }
-    
-    // }
-
-    // const rockBtn = document.querySelector('#rock-btn');
-// const paperBtn = document.querySelector('#paper-btn');
-// const scissorsBtn = document.querySelector('#scissors-btn');
-
-
-// const roundMsg = document.createElement('p');
-// const playerScoreMsg = document.createElement('p');
-// const computerScoreMsg = document.createElement('p');
-// const lineBreak = document.createElement('br');
-// const finalMsg = document.createElement('p');
-// const playAgainBtn = document.createElement('button');
-// playAgainBtn.textContent = 'Play Again';
